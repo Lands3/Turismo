2dba4e1639d1457458b26b643a1e95a1fe34c41c
. . <alanss2430@gmail.com>
2024-07-31 20:10:58 -0500

    Modificacion final en cuanto a los estilos nuevas funcionalidades y correcion de responsividad más nuevas funciones y eliminacion de partes innecesarias

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,54 @@
-
+// Inicialización de AOS
 AOS.init({
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 900, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  offset: 120,
+  delay: 0,
+  duration: 900,
+  easing: 'ease',
+  once: false,
+  mirror: false,
+  anchorPlacement: 'top-bottom',
+});
 
+document.addEventListener('DOMContentLoaded', function () {
+    const counters = document.querySelectorAll('#counter .display-4');
+    
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+    };
+
+    const callback = function (entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const counter = entry.target;
+                const target = +counter.getAttribute('data-target');
+                const increment = target / 100;
+                
+                let count = 0;
+
+                const updateCount = () => {
+                    count += increment;
+                    if (count < target) {
+                        counter.textContent = Math.ceil(count) + (counter.dataset.suffix || '');
+                        requestAnimationFrame(updateCount);
+                    } else {
+                        counter.textContent = target + (counter.dataset.suffix || '');
+                    }
+                };
+
+                requestAnimationFrame(updateCount);
+                observer.unobserve(counter);
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    counters.forEach(counter => {
+        counter.dataset.target = parseInt(counter.textContent);
+        counter.dataset.suffix = counter.textContent.replace(/^\d+/, '');
+        counter.textContent = '0';
+        observer.observe(counter);
+    });
 });
